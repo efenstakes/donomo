47b36befb3a215f675cc54b20217f4450809138a
Efen Stakes <efenstakes101@gmail.com>
2019-10-05 14:35:07 +0300

    add database connection file

---
+++ connect.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+
+// allow `findOneAndUpdate()` and `findOneAndDelete()` to work
+mongoose.set('useFindAndModify', false)
+
+const connect = async ()=> {
+    let connection 
+
+    try{
+        connection = await mongoose.connect('mongodb://localhost:27017/donomo', { useNewUrlParser: true })
+        console.log('connected to db')
+    }catch(e){
+        console.log('error connecting to db')
+    }
+
+    return connection
+}
+
+let connection = connect()
+module.exports = connection
